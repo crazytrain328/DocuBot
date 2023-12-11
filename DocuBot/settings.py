30febe74860cea5bdbce5b3442dad4c62f6bbca3
Joydeep Tripathy <113792434+crazytrain328@users.noreply.github.com>
2023-12-11 23:35:49 +0530

    fix: Changed style and Multiple Import fixes

--- DocuBot/settings.py
+++ DocuBot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,9 +122,7 @@ STATIC_URL = '/static/'
 MEDIA_URL= '/images/'
 
 
-STATICFILES_DIRS=[
-    BASE_DIR / 'static'
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
